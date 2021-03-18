3d2c7f05d63897d2f691b10e5dfe4de5ff7159bb
Gabriel Lopes <gabriel_lopes1910@outlook.com>
2021-03-18 10:00:23 -0300

    Challenge two done

---
+++ challenge-two/src/components/SideBar.tsx
@@ -0,0 +1,24 @@
+import { Button } from "./Button";
+import { useMovies } from "../MoviesContext";
+
+export function SideBar() {
+  const { genres, handleClickButton, selectedGenreId} = useMovies();
+  return (
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          id={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+
+  </nav>
+  )
+}
